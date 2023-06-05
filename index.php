e1489db165e6e1a5a0305515c5a13b0e10416d7b
Juan Diego <juandiegoramirez098@gmail.com>
2023-06-05 15:23:01 -0500

    feat: :sparkles: ejercicio 13, arrays
    Crear una lista de satélites para un planeta

--- index.php
+++ index.php
@@ -4,60 +4,20 @@
       ! Crear una lista de satélites para un planeta
     */
 
-    session_start();
 
-    isset($_SESSION["planetas"])
-      ? $_SESSION["planetas"]
-      : $_SESSION["planetas"] = ["mercurio" => Array(), "venuz" => Array(),  "tierra" => Array(),  "marte" => Array(),  "jupiter" => Array(),  "saturno" => Array(),  "urano" => Array(),  "neptuno" => Array()];
+    $planetas = ["mercurio", "venuz",  "tierra",  "marte",  "jupiter",  "saturno",  "urano",  "neptuno", "pluton"];
 
+    echo "Planetas en su orden => ";
+    foreach($planetas as $key){
 
-    if(isset($_POST["planeta"])){
+      echo $key.", ";
+    }
 
-      $planetas = $_SESSION["planetas"];
-      $planeta = $_POST["planeta"];
-      $satelite = $_POST["satelite"];
+    echo "<br/>";
 
-      array_push($planetas[$planeta], $satelite);
-      $_SESSION["planetas"] = $planetas;
+    echo "Planetas en su orden invertido => ";
+    foreach(array_reverse($planetas) as $key){
+
+      echo $key.", ";
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/style.css">
-  <title>Ejercicio 12</title>
-</head>
-<body>
-    <h1>Ejercicio 12</h1>
-    <form method="post">
-      <label for="planeta">PLanetas: </label>
-      <select name="planeta" id="planeta">
-        <?php foreach($_SESSION["planetas"] as $key => $value ){ ?>
-
-            <option value="<?php echo $key; ?>" ><?php echo $key; ?></option>
-          <?php } ?>
-      </select>
-      <label for="satelite">Satelite</label>
-      <input type="text" id="satelite" name="satelite">
-      <input type="submit" value="Agregar">
-    </form>
-
-    <section>
-    <br/>
-    <?php foreach($_SESSION["planetas"] as $key => $value ){ ?>
-
-        <b>Planeta: </b><?php echo $key; ?> <b>Satelites: </b>
-
-        <?php foreach($_SESSION["planetas"][$key] as $i => $value_satelite ){ ?>
-              <?php echo $value_satelite.","; ?>
-          <?php } ?>
-        <br/>
-        <br/>
-      <?php } ?>
-    </section>
-</body>
-</html>
